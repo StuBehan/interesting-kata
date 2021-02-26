20e49f4c483855d45606d29a35165a0ca61acaec
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-02-26 11:41:44 +0000

    added reversing of the IPv4 address to 32-bit int

--- int32_to_ip.rb
+++ int32_to_ip.rb
@@ -1,5 +1,11 @@
+# changing a 32-bit unsigned int to IPv4 address
 def int32_to_ip(int32)
-  puts [int32].pack('N').unpack('CCCC').join(".")
+  return [int32].pack('N').unpack('CCCC').join(".")
 end
 
-int32_to_ip(2154959208)
+# changing IPv4 address to 32-bit unsigned int 
+def ip_to_int32(ip)
+  return ip.split(".").map(&:to_i).pack('CCCC').unpack('N')
+end
+
+puts ip_to_int32((int32_to_ip(2154959208)))
